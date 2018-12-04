35411a43a78f37026a248a1761b59e87531db216
Mikkel Kjær Jensen <kjmikkel@gmail.com>
2018-12-04 12:34:02 +0100

    Improved the CompileAll script so it can work right out of the box

--- CompileAll.bat
+++ CompileAll.bat
@@ -2,10 +2,13 @@ DEL /q GOG\.
 DEL /q Steam\.
 DEL /q Result\.
 
+REM We need to get to use the correct command line utility
+CALL "D:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
+
 CALL GOG.bat
-START /w "D:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat" msbuild /t:rebuild /m
+msbuild /t:rebuild /m
 COPY result\. GOG\.
 
 CALL Steam.bat
-START /w "D:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat" msbuild /t:rebuild /m
+msbuild /t:rebuild /m
 COPY result\. Steam\.
